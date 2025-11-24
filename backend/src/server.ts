5081bcb86ab440041ad904fd575db231f0290188
Sambit Basant Naik <sambitbasantnaik@gmail.com>
2025-11-24 20:43:48 +0530

    changes in the backend for deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,16 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import path from 'path';
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
 dotenv.config();
 
-import { testConnection } from './config/db';
-import app from './app';
+import { testConnection } from "./config/db";
+import app from "./app";
 const PORT = process.env.PORT ? Number(process.env.PORT):5000;
 
-const frontendBuildPath = path.join(__dirname,"../../frontend/.next");
+const frontendBuildPath = path.join(__dirname,"../../frontend/out");
 app.use(express.static(frontendBuildPath));
 
-app.get(/.*/,(req,res)=>{
+app.get(/.*/,(_req,res)=>{
     res.sendFile(path.join(frontendBuildPath,"index.html"));
 })
 async function start(){
